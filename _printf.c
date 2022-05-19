ad655e62fbac2a5109cbb344b543565bdb0876c6
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-05-19 09:39:59 +0100

    created the _printf func and file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _printf - writes the character c to stdout
+ * @format: The format of the character to print
+ * @...: The variable arguments
+ *
+ * Return: int the number of character dispalyed.
+ */
+int _printf(const char *format, ...)
+{
+
+}
