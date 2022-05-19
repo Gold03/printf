64902048b8e1ccf14dc5ec7019a132b1b6450b8f
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-05-19 04:18:06 -0700

    betty fixes on _printf.c

--- _printf.c
+++ _printf.c
@@ -11,47 +11,45 @@
  */
 int _printf(const char *format, ...)
 {
-int i = 0, k = 0;
-va_list args;
-char *str = NULL;
+	int i = 0, k = 0;
+	va_list args;
+	char *str = NULL;
 
-va_start(args, format);
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+		else
+		{
+			if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				i++;
+			}
+			else if (format[i + 1] == 's')
+			{
+				i++;
+				str = va_arg(args, char *);
+				k = 0;
+				while (str[k] != '\0')
+				{
+					_putchar(str[k]);
+					k++;
+				}
+			}
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				i++;
+			}
+		}
+		i++;
+	}
 
-while (format[i] != '\0')
-{
-    if (format[i] != '%')
-    {
-         _putchar(format[i]);
-    }
-    else
-    {
-    if(format[i + 1] == 'c')
-    {
-         _putchar(va_arg(args, int));
-        i++;
-    }
-    else if (format[i + 1] == 's')
-    {
-        i++;
-        str = va_arg(args, char *);
-        k = 0;
-        while(str[k] != '\0')
-        {
-            _putchar(str[k]);
-            k++;
-        }
-    }
-    else if (format[i + 1] == '%')
-    {
-         _putchar('%');
-        i++;
-    }
-        
-    }
-  i++;      
+	va_end(args);
+	return (0);
 }
 
-va_end(args);
-
-return (0);
-}
